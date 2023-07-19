db14a0e75d3ab5340bc247d84e040d1091bc6fbe
Laurence Juden <juden@cngroup.dk>
2023-07-19 16:00:20 +0200

    Set up express with an endpoint for prompts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hi");
+import express, { Request, Response } from 'express';
+import { Prompt } from './types';
+
+const app = express();
+const port = 8000;
+
+app.get('/prompts', (req: Request, res: Response) => {
+  const data: Prompt[] = [
+    { id: 1, userId: 1, prompt: 'What is your name?' },
+    { id: 2, userId: 1, prompt: 'What is your quest?' },
+  ];
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
