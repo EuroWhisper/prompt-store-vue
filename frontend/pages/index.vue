b4a28e89b40de570ed56b7489ff059391f0d764a
Laurence Juden <juden@cngroup.dk>
2023-07-20 15:01:36 +0200

    Add index page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Prompt List</h1>
+    <div v-for="prompt in prompts">
+      <p>ID: {{ prompt.id }}</p>
+      <p>User: {{ prompt.userId }}</p>
+      <p>Prompt: {{ prompt.prompt }}</p>
+    </div>
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Prompt } from 'types';
+
+const {
+  data: prompts,
+  pending,
+  error,
+} = await useFetch<Prompt[]>('http://localhost:8000/prompts');
+</script>
